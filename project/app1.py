a975ec2bcae22f337c8634b27cf5bf9a19c321e4
mx-nika46 <kerecnika0@gmail.com>
2026-04-07 08:39:12 +0000

    1 naloga

--- project/app1.py
+++ project/app1.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template, request, session
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect, session, jsonify
+from tinydb import TinyDB, Query
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/notes.db'
-db = SQLAlchemy(app)
+app = Flask( __name__, template_folder="templates1", static_folder="static1")
+app.secret_key = "skrivnost123"
 
+db = TinyDB("db.json")
+users = db.table("users")
+
+User = Query()
 
 # homepage
 @app.route("/")
@@ -12,3 +16,43 @@ def home():
         return redirect("/dashboard")
     return redirect("/login")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if users.search(User.username == username):
+            return "Uporabnik že obstaja"
+        
+        users.insert({"username" : username, "password" : password, "note" : ""})
+        return redirect("/login")
+
+        # print(username, password)
+    return render_template("register.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = users.get(User.username == username)
+        #print(user)
+
+        if user and user["password"] == password:
+            session["user"] = username 
+            return redirect("/dashboard")
+        
+        return "Napačen login"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect("/login")
+    return render_template("dashboard.html", user = session["user"])
+
+app.run(debug=1)
